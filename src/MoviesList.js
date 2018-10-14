5ceaef0fff933afb217247aafba761793e8a8627
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-14 16:08:49 +0300

    Testing with testId

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -11,7 +11,7 @@ class MoviesList extends PureComponent {
   async componentDidMount() {
     try {
       const res = await fetch(
-        'https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+        'https://api.themoviedb.org/3/discover/movie?api_key=hi&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
       );
       const movies = await res.json();
       this.setState({
