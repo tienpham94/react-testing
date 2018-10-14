5d6d19137469ef36988df35feed712cbb1376c49
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-14 19:59:36 +0300

    Testing for errors

--- src/Movie.js
+++ src/Movie.js
@@ -6,19 +6,24 @@ import Overdrive from 'react-overdrive';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
-const Movie = ({ movie }) => (
-  <Link to={`/${movie.id}`}>
-    <Overdrive id={`${movie.id}`}>
-      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-    </Overdrive>
-  </Link>
-);
+const Movie = ({ movie }) => {
+  if (!movie) return null;
+  return (
+    <Link to={`/${movie.id}`}>
+      <Overdrive id={`${movie.id}`}>
+        <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+      </Overdrive>
+    </Link>
+  );
+};
 
 export default Movie;
 
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
