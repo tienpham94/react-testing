3a87764fff8fac1a690b5d8bbc904b940941ecdd
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-14 20:15:31 +0300

    Test loading state

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -23,9 +23,14 @@ class MoviesList extends PureComponent {
   }
 
   render() {
+    const { movies } = this.state;
+
+    if (movies < 1) return <h1 data-testid="loading">Loading</h1>;
     return (
       <MovieGrid>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </MovieGrid>
     );
   }
