83a614061fbcaa4fdbaeaac58018b0801c2910ba
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-14 16:17:07 +0300

    Events In React Testing Library

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Counter from './Counter';
+
+afterEach(cleanup);
 
 test('<Counter />', () => {
   // Renders component
   const { debug, getByTestId } = render(<Counter />);
 
-  debug(); // Outputs dom as string
+  // debug(); // Outputs dom as string
+  const counterButton = getByTestId('counter-button');
 
   // Asserts counter-button is a button
-  expect(getByTestId('counter-button').tagName).toBe('BUTTON');
+  expect(counterButton.tagName).toBe('BUTTON');
   // Asserts counter-button starts at 0
-  expect(getByTestId('counter-button').textContent).toBe('0');
+  expect(counterButton.textContent).toBe('0');
+
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('1');
+
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('2');
+  debug(); // Outputs dom as string
 });
